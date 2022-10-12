63e4b639f25784a465c381641b0adf122b84b500
jay kumar <kumarjay19260@gmail.com>
2022-10-12 11:56:10 +0530

    First Commit

---
+++ demo2.java
@@ -0,0 +1,9 @@
+public class name{
+	public static void main( String []args){
+		System.out.println("Hello World !");
+
+		system.out.println("bhabi tera devar diwana!");
+		system.out.println("ram nam satya hai !");
+	}
+}
+
